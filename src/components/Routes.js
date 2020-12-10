5b7b2e6c16e83d9b6f11c3de1701f2051cca1e11
Nicolas Sawczuk <nico.290698@hotmail.com>
2020-12-10 17:00:51 -0300

    Rutas para usuarios

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Route, Switch } from "wouter";
 import Home from '../pages/Home'
+import Users from '../pages/Users'
 
 export default function Routes() {
     return (
         <>
             <Switch>
                 <Route component={Home} path="/" />
+                <Route component={Users} path="/users" />
             </Switch>
         </>
     )
